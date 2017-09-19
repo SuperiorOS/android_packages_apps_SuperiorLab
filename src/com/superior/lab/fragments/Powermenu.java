0d8d3afdf68d72294ef1d29e49e7e441f2d04b40
Alex Cruz <mazdarider23@gmail.com>
2017-09-19 19:17:56 +0800

    SuperiorLab: Advanced powermenu cust [2/2]
    sagarmakhar: migrate to androidx
    
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I9659e31b9e8d59c5c16f474ae79b0919bf2a9286
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- src/com/superior/lab/fragments/Powermenu.java
+++ src/com/superior/lab/fragments/Powermenu.java
@@ -41,7 +41,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class PowerMenuSettings extends SettingsPreferenceFragment
+public class Powermenu extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
 
@@ -49,7 +49,7 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.superior_lab_power);
+        addPreferencesFromResource(R.xml.powermenu);
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
