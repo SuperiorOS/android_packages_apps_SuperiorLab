3154ed883f1dbadebe95a7984721e6b8cbf00d95
AnierinB <anierin@evolution-x.org>
2024-08-15 10:32:42 -0400

    SuperiorLab: PixelProps Changes
    reverts https://github.com/SuperiorOS/android_packages_apps_SuperiorLab/commit/993ef3d1c231bc6292516765a31b06bbca5c69b7
    
    Change-Id: I81723b1a6e4ba5c93db1317e137140f661b7363b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

---
+++ src/com/superior/lab/fragments/Spoofing.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2019-2024 The Evolution X Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.superior.lab.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.List;
+
+import com.superior.support.preferences.SystemPropertySwitchPreference;
+
+@SearchIndexable
+public class Spoofing extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "Spoofing";
+
+    private static final String KEY_SYSTEM_WIDE_CATEGORY = "spoofing_system_wide_category";
+    private static final String KEY_PIXEL_PROPS = "persist.sys.pphooks.enable";
+
+    private PreferenceCategory mSystemWideCategory;
+    private SystemPropertySwitchPreference mPixelProps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.spoofing);
+
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources resources = context.getResources();
+
+        mSystemWideCategory = (PreferenceCategory) findPreference(KEY_SYSTEM_WIDE_CATEGORY);
+        mPixelProps = (SystemPropertySwitchPreference) findPreference(KEY_PIXEL_PROPS);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.SUPERIOR;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider(R.xml.spoofing) {
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                final Resources resources = context.getResources();
+                return keys;
+            }
+        };
+}
